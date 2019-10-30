e1bc749ef2a4ba659ba5e53df13ac9491a8f7853
Chaplygin-vlad <metro_flava@mail.ru>
2019-10-30 17:43:04 +0300

    Найдены все интересующие элементы на HTML странице. Пока они просто принтуются по очереди. Так же была добавлена функция, для того что бы понять есть ли данный элемент на странице или нет. Если нет - выводиться сообщение (например: Комментарии отсутсвуют). Для удобства посты и комментарии нумеруются

--- pars.py
+++ pars.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
-
+def check_exists_by_xpath(xpath):
+    return len(driver.find_elements_by_xpath(xpath)) > 0
 #from selenium.webdriver.common.keys import Keys
 urls=[]
 driver = webdriver.Firefox()
-driver.get('https://www.reddit.com/search/?q=москва')
+driver.get('https://www.reddit.com/search/?q=план маршала')
 news=driver.find_elements_by_xpath("//a[@class='SQnoC3ObvgnGjWt90zD9Z _2INHSNB8V5eaWp4P0rY_mE']")
+b=1
+c = 1
 for i in news[:5]:
     a=i.get_attribute('href')
     url= {
@@ -14,7 +17,41 @@ for i in news[:5]:
 for j in urls:
     driver.get(j['href'])
     ttl=driver.find_element_by_xpath("//h1[@class='_eYtD2XCVieq6emjKBH3m']")
-    com=driver.find_elements_by_xpath("//p[@class='_1qeIAgB0cPwnLhDF9XSiJM']")
-    print (ttl.text)
-    print (com[:5]) #ищет пока просто элементы
+    #com=driver.find_elements_by_xpath("//p[@class='_1qeIAgB0cPwnLhDF9XSiJM']")
+    com=driver.find_elements_by_xpath("//div[@class='_3tw__eCCe7j-epNCKGXUKk ']/div/div/p")
+    pic_check=check_exists_by_xpath("//div[@class='_3Oa0THmZ3f5iZXAQ0hBJ0k ']//a")
+    lnk_check=check_exists_by_xpath("//a[@class='_13svhQIUZqD9PVzFcLwOKT styled-outbound-link']")
+    com_check=check_exists_by_xpath("//div[@class='_3tw__eCCe7j-epNCKGXUKk ']/div/div/p")
+    txt_check=check_exists_by_xpath("//div[@data-click-id='text']/div/p")
+    print ("Заголовок"+str(b)+": "+ttl.text)
+    if pic_check > 0:
+        pic=driver.find_element_by_xpath("//div[@class='_3Oa0THmZ3f5iZXAQ0hBJ0k ']//a")
+        print (pic.get_attribute('href'))
+    else:
+        print ("Изображений нет")
+    if lnk_check > 0:
+        lnk=driver.find_element_by_xpath("//a[@class='_13svhQIUZqD9PVzFcLwOKT styled-outbound-link']")
+        print (lnk.get_attribute('href'))
+    else:
+        print ("Ссылок нет")
+    if txt_check > 0:
+        txt=driver.find_elements_by_xpath("//div[@data-click-id='text']/div/p")
+        for f in txt:
+            print(f.text)
+    else:
+        print("Текст поста отсутствует")
+    if com_check > 0:
+        c=1
+        for h in com[:5]:
+            print ("Комментарий " +str(c)+": "+h.text)
+            c=c+1
+    else:
+        print ("Комментарии отсутсвуют")
+    print ("\n \n \n")
+    b+=1
+
+
+#_13svhQIUZqD9PVzFcLwOKT styled-outbound-link - a класс где ссылка в теле
+#<div class="_3Oa0THmZ3f5iZXAQ0hBJ0k - div класс где картинка
+
 
