4d4e5b2d54dfc74f4b2b9da6b1802943d261e57e
Chaplygin-vlad <metro_flava@mail.ru>
2019-11-01 14:30:39 +0300

    В данной версии были сделаны следующие изменения: 1.Было реализовано подключение к базе данных и запись в нее всех собранных данных с сайта reddit.com 2. Исправлены ошибки написания имен переменных. 3. Отредактировано по стандарду PEP8 4. Отказ от консрукции if для проверки нахождения в HTML нужного элемента. Замена на конструкцию try 5. Реализовано промежуточное хранение данных с исследуемого сайта в словаре и дальнейшее заполнение базы данных из словаря.
    Вопросы к решению:
    1. На данный момент текст и комменнтарии вставляются в базу данных как
    список. Нужно подумать будет ли это играть роль при заполении карточек
    Trello и при надобности объединить в единый текст.
    2. Создание вебсервера
    3. Реализация заполнения карточек Trello используя модуль API trello

--- pars.py
+++ pars.py
@@ -1,57 +1,136 @@
 from selenium import webdriver
-def check_exists_by_xpath(xpath):
-    return len(driver.find_elements_by_xpath(xpath)) > 0
-#from selenium.webdriver.common.keys import Keys
-urls=[]
+import psycopg2
+
+
+def write_pstgres(data):
+    DB_NAME = "ffcvdwpe"
+    DB_USER = "ffcvdwpe"
+    DB_PASS = "ehGq2l8TRqXPmdPRmhOr6hy5D2zwoBNt"
+    DB_HOST = "salt.db.elephantsql.com"
+    DB_PORT = "5432"
+
+    conn = psycopg2.connect(database=DB_NAME, user=DB_USER,
+                            password=DB_PASS, host=DB_HOST, port=DB_PORT)
+
+    cursor = conn.cursor()
+
+    _SQL = "insert into reddit (header, image, news_link, all_text,comments) values (%s,%s,%s,%s,%s)"
+
+    cursor.execute(_SQL, (data['header'],
+                          data['image'],
+                          data['news_link'],
+                          data['all_text'],
+                          data['comments']))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+def drop_sql():
+    DB_NAME = "ffcvdwpe"
+    DB_USER = "ffcvdwpe"
+    DB_PASS = "ehGq2l8TRqXPmdPRmhOr6hy5D2zwoBNt"
+    DB_HOST = "salt.db.elephantsql.com"
+    DB_PORT = "5432"
+
+    conn = psycopg2.connect(database=DB_NAME, user=DB_USER,
+                            password=DB_PASS, host=DB_HOST, port=DB_PORT)
+
+    cur = conn.cursor()
+    try:
+        cur.execute("drop table reddit")
+    except Exception:
+        print()
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+def create_sql():
+    DB_NAME = "ffcvdwpe"
+    DB_USER = "ffcvdwpe"
+    DB_PASS = "ehGq2l8TRqXPmdPRmhOr6hy5D2zwoBNt"
+    DB_HOST = "salt.db.elephantsql.com"
+    DB_PORT = "5432"
+
+    conn = psycopg2.connect(database=DB_NAME, user=DB_USER,
+                            password=DB_PASS, host=DB_HOST, port=DB_PORT)
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+
+        CREATE TABLE Reddit
+        (
+        HEADER TEXT NOT NULL,
+        IMAGE TEXT NOT NULL,
+        NEWS_LINK TEXT NOT NULL,
+        ALL_TEXT TEXT NOT NULL,
+        COMMENTS TEXT NOT NULL
+        )
+
+        """)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+urls = []
 driver = webdriver.Firefox()
 driver.get('https://www.reddit.com/search/?q=план маршала')
-news=driver.find_elements_by_xpath("//a[@class='SQnoC3ObvgnGjWt90zD9Z _2INHSNB8V5eaWp4P0rY_mE']")
-b=1
-c = 1
+news = driver.find_elements_by_xpath("//a[@class='SQnoC3ObvgnGjWt90zD9Z _2INHSNB8V5eaWp4P0rY_mE']")
+all_text = []
+comments = []
+drop_sql()
+create_sql()
 for i in news[:5]:
-    a=i.get_attribute('href')
-    url= {
+    a = i.get_attribute('href')
+    url = {
         'href': a
     }
     urls.append(url)
 for j in urls:
     driver.get(j['href'])
-    ttl=driver.find_element_by_xpath("//h1[@class='_eYtD2XCVieq6emjKBH3m']")
-    #com=driver.find_elements_by_xpath("//p[@class='_1qeIAgB0cPwnLhDF9XSiJM']")
-    com=driver.find_elements_by_xpath("//div[@class='_3tw__eCCe7j-epNCKGXUKk ']/div/div/p")
-    pic_check=check_exists_by_xpath("//div[@class='_3Oa0THmZ3f5iZXAQ0hBJ0k ']//a")
-    lnk_check=check_exists_by_xpath("//a[@class='_13svhQIUZqD9PVzFcLwOKT styled-outbound-link']")
-    com_check=check_exists_by_xpath("//div[@class='_3tw__eCCe7j-epNCKGXUKk ']/div/div/p")
-    txt_check=check_exists_by_xpath("//div[@data-click-id='text']/div/p")
-    print ("Заголовок"+str(b)+": "+ttl.text)
-    if pic_check > 0:
-        pic=driver.find_element_by_xpath("//div[@class='_3Oa0THmZ3f5iZXAQ0hBJ0k ']//a")
-        print (pic.get_attribute('href'))
-    else:
-        print ("Изображений нет")
-    if lnk_check > 0:
-        lnk=driver.find_element_by_xpath("//a[@class='_13svhQIUZqD9PVzFcLwOKT styled-outbound-link']")
-        print (lnk.get_attribute('href'))
-    else:
-        print ("Ссылок нет")
-    if txt_check > 0:
-        txt=driver.find_elements_by_xpath("//div[@data-click-id='text']/div/p")
-        for f in txt:
-            print(f.text)
-    else:
-        print("Текст поста отсутствует")
-    if com_check > 0:
-        c=1
-        for h in com[:5]:
-            print ("Комментарий " +str(c)+": "+h.text)
-            c=c+1
-    else:
-        print ("Комментарии отсутсвуют")
-    print ("\n \n \n")
-    b+=1
+    try:
+        header = driver.find_element_by_xpath("//h1[@class='_eYtD2XCVieq6emjKBH3m']").text
+    except Exception:
+        header = ' '
+    try:
+        image = driver.find_element_by_xpath("//div[@class='_3Oa0THmZ3f5iZXAQ0hBJ0k ']//a").get_attribute('href')
+    except Exception:
+        image = 'Изображение отсутсвует'
+    try:
+        news_link = driver.find_element_by_xpath(
+            "//a[@class='_13svhQIUZqD9PVzFcLwOKT styled-outbound-link']").get_attribute('href')
+    except Exception:
+        news_link = 'Ссылка на внешний источник отсутсвует'
+    try:
+        text = driver.find_elements_by_xpath("//div[@data-click-id='text']/div/p")
+        for i in text:
+            all_text.append(i.text)
+        if all_text == []:
+            all_text = 'Текст поста отсутствует'
+    except Exception:
+        all_text = 'Текст поста отсутсвует'
+    try:
+        comment = driver.find_elements_by_xpath("//div[@class='_3tw__eCCe7j-epNCKGXUKk ']/div/div/p")
+        for i in comment[0:5]:
+            comments.append(i.text)
+        if comments == []:
+            comments = 'Комментарии отсутсвуют'
+    except Exception:
+        comments = 'Комментарии отсутсвуют'
 
-
-#_13svhQIUZqD9PVzFcLwOKT styled-outbound-link - a класс где ссылка в теле
-#<div class="_3Oa0THmZ3f5iZXAQ0hBJ0k - div класс где картинка
-
-
+    data = {'header': header,
+            'image': image,
+            'news_link': news_link,
+            'all_text': all_text,
+            'comments': comments,
+            }
+    # print(data)
+    all_text = []
+    comments = []
+    write_pstgres(data)
